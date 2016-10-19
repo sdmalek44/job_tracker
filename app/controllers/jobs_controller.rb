cce03811e834f08649c22db5c011f34d43a2f6b7
Casey Ann Cumbow <cumbow8@gmail.com>
2016-10-19 16:20:38 -0600

    Add remainder Job functionality w/out edit/update/destroy

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,4 +3,42 @@ class JobsController < ApplicationController
     @company = Company.find(params[:company_id])
     @jobs = @company.jobs
   end
+
+  def new
+    @company = Company.find(params[:company_id])
+    @job = Job.new()
+  end
+
+  def create
+    @company = Company.find(params[:company_id])
+    @job = @company.jobs.new(job_params)
+    if @job.save
+      flash[:success] = "You created #{@job.title} at #{@company.name}"
+      redirect_to company_job_path(@company, @job)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def edit
+    # implement on your own!
+  end
+
+  def update
+    # implement on your own!
+  end
+
+  def destroy
+    # implement on your own!
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description, :level_of_interest)
+  end
 end
