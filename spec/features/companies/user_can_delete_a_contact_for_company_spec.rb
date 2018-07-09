42634d7f5d0d67e71441ff5fa0d9d5537dec5bbd
Stephen Malek <malek.stephen44@gmail.com>
2018-07-09 17:28:01 -0600

    add delete contact button on company show page

---
+++ spec/features/companies/user_can_delete_a_contact_for_company_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'features' do
+  describe 'when a user visits /companies/#' do
+    it  'can delete a company' do
+      company = Company.create!(name: 'ESPN')
+      contact = company.contacts.create!(name: 'bobby', position: 'newscaster', email: 'dkflkj@yahoozle.com')
+
+      visit company_path(company)
+
+      expect(page).to have_content(contact.name)
+
+      click_on "Delete Contact"
+
+      expect(current_path).to eq(company_path(company))
+      expect(page).to_not have_content(contact.name)
+    end
+  end
+end
