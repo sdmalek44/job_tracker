a2c744dae590d95242ffd35a2f4ffd19c812acf3
Stephen Malek <malek.stephen44@gmail.com>
2018-07-09 19:17:52 -0600

    change dashboard method in controller to index

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   end
   resources :jobs, only: [:index]
   resources :categories
-  get '/dashboard' => 'dashboard#analysis'
+  get '/dashboard' => 'dashboard#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
