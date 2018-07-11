3f6dc06c137fdf213dfcdb46ab8961bb5acb9bfc
Colin Armstrong <colinwarmstrong@gmail.com>
2018-07-11 09:38:53 -0600

    Update user can add a contact spec.

--- spec/features/companies/user_can_add_contacts_to_companies_spec.rb
+++ spec/features/companies/user_can_add_contacts_to_companies_spec.rb
@@ -7,15 +7,15 @@ describe 'features' do
 
       visit company_path(company)
 
-      fill_in "contact[name]", with: "Chris Bishop"
-      fill_in "contact[position]", with: "Doctor"
-      fill_in "contact[email]", with: "cbishop@doctor.com"
-      click_on "Create Contact"
+      fill_in :contact_name, with: 'Chris Bishop'
+      fill_in :contact_position, with: 'Doctor'
+      fill_in :contact_email, with: 'cbishop@doctor.com'
+      click_on 'Create Contact'
 
       expect(current_path).to eq(company_path(company))
-      expect(page).to have_content("Chris Bishop")
-      expect(page).to have_content("Doctor")
-      expect(page).to have_content("cbishop@doctor.com")
+      expect(page).to have_content('Chris Bishop')
+      expect(page).to have_content('Doctor')
+      expect(page).to have_content('cbishop@doctor.com')
     end
   end
 end
