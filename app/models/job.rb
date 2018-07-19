68a486ccc25744c5fe9c186d187b7d4a1542f4bd
Stephen Malek <malek.stephen44@gmail.com>
2018-07-19 10:34:07 -0600

    add additional changes

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,10 +1,9 @@
 class Job < ApplicationRecord
   validates_presence_of :title, :level_of_interest, :city
-  
+
   belongs_to :company
   belongs_to :category
   has_many :comments, dependent: :destroy
-
 
   def self.count_levels_of_interest
     order(level_of_interest: :desc).group(:level_of_interest).count
